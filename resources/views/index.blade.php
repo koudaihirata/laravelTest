044a64b7f480233fb0a855660e852823536a99cb
koudaihirata <2230051@ecc.ac.jp>
2024-04-17 22:33:13 +0900

    修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,21 +6,6 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>My BBS</h1>
-    <ul>
-        <li>Title</li>
-        <li>Title</li>
-        <li>Title</li>
-    </ul>
-</body>
-</html><!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
     <div class="container">
         <h1>My BBS</h1>
         <ul>
