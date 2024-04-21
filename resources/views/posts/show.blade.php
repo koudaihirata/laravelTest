9772e189bbd56226c95ec2222a08924af37d3989
koudaihirata <2230051@ecc.ac.jp>
2024-04-22 00:50:29 +0900

    public フォルダからのパスを使って URL を指定する

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>My BBS</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 <body>
     <div class="container">
+        <div class="back-link">
+            &laquo; <a href="/">Back</a>
+        </div>
+
         <h1>{{ $post }}</h1>
     </div>
 </body>
