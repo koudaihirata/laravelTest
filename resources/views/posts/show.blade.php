6548caf89916c4ced62ee45301a2c08a310bbd1f
koudaihirata <2230051@ecc.ac.jp>
2024-04-22 00:56:49 +0900

    コンポーネントという仕組みを使って、Viewの共通部分を切り出す方法について見ていきます。

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        <div class="back-link">
-            &laquo; <a href="{{ route('posts.index') }}">Back</a>
-        </div>
-
-        <h1>{{ $post }}</h1>
+<x-layout>
+    <div class="back-link">
+        &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
-</body>
-</html>
+
+    <h1>{{ $post }}</h1>
+</x-layout>
